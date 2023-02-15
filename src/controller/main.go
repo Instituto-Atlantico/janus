4c8988b929c038c5fffdc84b22534a8832fe5052
Elton Lazzarin <elton_lazzarin@atlantico.com.br>
2023-02-15 14:45:12 -0300

    add proof incremental logic

--- src/controller/main.go
+++ src/controller/main.go
@@ -6,19 +6,21 @@ import (
 	"log"
 	"time"
 
-	"github.com/ldej/go-acapy-client"
+	"github.com/Instituto-Atlantico/go-acapy-client"
 )
 
 func main() {
 	//issuer creates an invitation
-	issuer := acapy.NewClient("http://192.168.229.8:8002/")
-	holder := acapy.NewClient("http://192.168.229.8:9002/")
+	issuer := acapy.NewClient("http://192.168.228.89:8002/")
+	holder := acapy.NewClient("http://192.168.228.89:9002/")
 
 	fmt.Println("Create Invitation")
 	invitation, err := issuer.CreateInvitation("createdByCodeTest", true, false, false)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(invitation.ConnectionID)
 
 	//holder receive the invitation
 	parsedInvitation, _ := json.Marshal(invitation.Invitation)
@@ -33,6 +35,8 @@ func main() {
 		log.Fatal(err)
 	}
 	_ = connection
+
+	fmt.Println(connection)
 
 	//issuer register a schema
 	schemaName := fmt.Sprintf("schema-elton-%v", time.Now().Unix())
@@ -57,11 +61,13 @@ func main() {
 	fmt.Println(credentialDefinition)
 
 	// get public DID
-	fmt.Println("Create Cred Definition")
+	fmt.Println("Issuer DID")
 	issuerDID, err := issuer.GetPublicDID()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(issuerDID.DID)
 
 	// issuer issues a credential
 	attributes := []acapy.CredentialPreviewAttributeV2{
@@ -79,10 +85,82 @@ func main() {
 
 	credentialPreview := acapy.NewCredentialPreviewV2(attributes)
 
-	fmt.Println("Proposal Credential")
-	holder.ProposeCredentialV2(connection.ConnectionID, credentialPreview, "first comment", credentialDefinition, issuerDID.DID, schema.ID)
+	fmt.Println("Offer Credential")
+	offerCredential, err := issuer.OfferCredentialV2(invitation.ConnectionID, credentialPreview, credentialDefinition, "first comment bellow")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(offerCredential)
 
 	// holder sends a presentation
+	requestedPredicates := map[string]acapy.RequestedPredicate{
+		"age": acapy.RequestedPredicate{
+			Restrictions: []acapy.Restrictions{{ // Required in case of Names
+				CredentialDefinitionID: credentialDefinition,
+			}},
+			Name:   "age", // XOR with Names
+			PType:  acapy.PredicateGT,
+			PValue: 18,
+			NonRevoked: acapy.NonRevoked{
+				From: time.Now().Add(-time.Hour * 24 * 7).Unix(),
+				To:   time.Now().Unix(),
+			},
+		},
+	}
+
+	requestedAttributes := map[string]acapy.RequestedAttribute{
+		"name": acapy.RequestedAttribute{
+			Restrictions: []acapy.Restrictions{{ // Required in case of Names
+				CredentialDefinitionID: credentialDefinition,
+			}},
+			Name: "name", // XOR with Names
+			NonRevoked: acapy.NonRevoked{
+				From: time.Now().Add(-time.Hour * 24 * 7).Unix(),
+				To:   time.Now().Unix(),
+			},
+		},
+	}
+
+	presentationRequestRequest := acapy.PresentationRequestRequest{
+		Trace:        false,
+		Comment:      "Presentation request test",
+		ConnectionID: invitation.ConnectionID,
+		ProofRequest: acapy.NewProofRequest(
+			"Elton",
+			"1234567890",
+			requestedPredicates,
+			requestedAttributes,
+			"1.0",
+			&acapy.NonRevoked{
+				From: time.Now().Add(-time.Hour * 24 * 7).Unix(),
+				To:   time.Now().Unix(),
+			},
+		),
+	}
+
+	fmt.Println("Send Presentation Request")
+	sendPresentation, err := issuer.SendPresentationRequest(presentationRequestRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(sendPresentation)
+
+	// Query presentation ID
+	fmt.Println("Query presentation ID")
+	param := acapy.QueryPresentationExchangeParams{
+		//ConnectionID: connection.ConnectionID,
+	}
+
+	presentation, err := holder.QueryPresentationExchange(param)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(presentation)
+
+	// holder send presentation
+	//holder.SendPresentationByID()
 
 	// issuer verifies
 }
