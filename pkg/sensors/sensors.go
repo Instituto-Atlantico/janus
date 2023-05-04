063d41a60b55bc68157654ac3beaec6bc8226f52
Elton Lazzarin <elton_lazzarin@atlantico.com.br>
2023-05-04 11:12:48 -0300

    add sensor error validation

--- pkg/sensors/sensors.go
+++ pkg/sensors/sensors.go
@@ -2,21 +2,31 @@ package sensors
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"syscall"
 )
 
 func CollectSensorData(ip, port string) (map[string]any, error) {
 	resp, err := http.Get(fmt.Sprintf("http://%s:%s", ip, port))
+	connectionRefused := errors.Is(err, syscall.ECONNREFUSED)
+	if err != nil && connectionRefused {
+		err := errors.New("Error requesting sensor data. The API doesn't seem to be working properly. Check its availability")
+
+		return nil, err
+	}
+
 	if err != nil {
 		return nil, err
 	}
 
 	//{"temperature": "10", "humidity": "68"}
-
 	data := make(map[string]any)
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
+		err := errors.New("Error in reading sensor data. Check its physical integrity")
+
 		return nil, err
 	}
 
