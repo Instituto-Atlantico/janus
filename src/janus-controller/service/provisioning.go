757f027a1f889a516c800d8dcd0da34dc89f77fb
Elton Lazzarin <elton_lazzarin@atlantico.com.br>
2023-05-12 11:55:44 -0300

    add properties examples

--- src/janus-controller/service/provisioning.go
+++ src/janus-controller/service/provisioning.go
@@ -1,11 +1,11 @@
 package service
 
 type ProvisionBody struct {
-	DeviceHostName string   `json:"deviceHostName"` // user@ip
-	Permissions    []string `json:"permissions"`    // ["temperature", "humidity"]
-	BrokerIp       string   `json:"brokerIp"`       // 127.0.0.1
-	BrokerUsername string   `json:"brokerUsername"` // brokerUser:deviceID
-	BrokerPassword string   `json:"brokerPassword"` // brokerPass
+	DeviceHostName string   `json:"deviceHostName" example:"rasp@192.168.0.5"`   // user@ip
+	Permissions    []string `json:"permissions" example:"temperature, humidity"` // ["temperature", "humidity"]
+	BrokerIp       string   `json:"brokerIp" example:"192.168.0.2"`              // 127.0.0.1
+	BrokerUsername string   `json:"brokerUsername" example:"admin:a1998e"`       // brokerUser:deviceID
+	BrokerPassword string   `json:"brokerPassword" example:"admin"`              // brokerPass
 }
 
 func ProvisionBodyIsValid(body ProvisionBody) bool {
